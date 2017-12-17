c25c19733735884a02b899c93bc74cec87cfebdd
CarlosGomes98 <carlosmiguel.gomes@live.com.pt>
2017-12-17 16:47:35 +0000

    Finally ignored .vscode files

---
+++ Graph/TestGraph.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TestGraph{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Node<Integer> graph = new Node<Integer>(20);
+
+        graph.addConnection(Arrays.asList(30, 40, 50));
+        graph.getConnections().get(1).addConnection(45);
+        graph.printGraph();
+        System.out.println(graph.DFS(scan.nextInt()));
+        System.out.println(graph.BFS(scan.nextInt()));
+        scan.close();
+    }
+}
